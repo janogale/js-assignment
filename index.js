c459e5c813e29e098f35cdf7624960c31d0d0baa
Asma mohamed <67588350+asma-ux@users.noreply.github.com>
2021-06-29 03:55:17 -0700

    finished

---
+++ index.js
@@ -0,0 +1,16 @@
+discount = 0.15;
+let bill = 102;
+let tip = discount * bill;
+console.log(tip);
+total = bill + tip;
+
+console.log(total);
+
+result =
+  "the bill was " +
+  bill +
+  ",the tip was " +
+  tip +
+  ",and the total values " +
+  total;
+console.log(result);
